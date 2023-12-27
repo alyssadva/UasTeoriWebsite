157a5e994f14965d2807bb87dffe7915e2d4dfe2
alyssadva <128569970+alyssadva@users.noreply.github.com>
2023-12-27 16:27:24 +0700

    Update hapusproduk.php

--- hapusproduk.php
+++ hapusproduk.php
@@ -1,14 +1,24 @@
 <?php 
 session_start();
-$ambil=$koneksi->query("SELECT * FROM produk WHERE id_produk='$_GET[id]'");
-$pecah=$ambil->fetch_assoc();
-$fotoproduk=$pecah['foto_produk'];
+// Filter berdasarkan id_produk yang diterima dari $_GET
+$filter = ['id_produk' => $_GET['id']];
+
+// Mendapatkan detail produk sebelum menghapus
+$query = new MongoDB\Driver\Query($filter);
+$cursor = $manager->executeQuery('nama_koleksi_produk', $query); // Gantikan 'nama_koleksi_produk' dengan nama koleksi Anda
+$pecah = current($cursor->toArray());
+
+// Menghapus file gambar produk jika ada
+$fotoproduk = $pecah->foto_produk;
 if (file_exists("../foto_produk/$fotoproduk")) {
-	unlink("../foto_produk/$fotoproduk");
+    unlink("../foto_produk/$fotoproduk");
 }
 
-$koneksi->query("DELETE FROM produk WHERE id_produk='$_GET[id]'");
+// Menghapus data produk dari MongoDB
+$bulkWrite = new MongoDB\Driver\BulkWrite();
+$bulkWrite->delete($filter, ['limit' => 1]); // Menghapus satu dokumen yang sesuai dengan filter
+$manager->executeBulkWrite('nama_koleksi_produk', $bulkWrite); // Gantikan 'nama_koleksi_produk' dengan nama koleksi Anda
 
-echo "<script>alert('produk terhapus');</script>";
+echo "<script>alert('Produk terhapus');</script>";
 echo "<script>location='index.php?halaman=produk';</script>";
 ?>

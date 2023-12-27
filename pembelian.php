6a4d8586e297665ab452f1da57f3e5d3342cea1e
alyssadva <128569970+alyssadva@users.noreply.github.com>
2023-12-27 16:35:18 +0700

    Update pembelian.php

--- pembelian.php
+++ pembelian.php
@@ -1,3 +1,9 @@
+<?php
+// Menghubungkan ke database MongoDB
+$manager = new MongoDB\Driver\Manager("mongodb+srv://alyssadiva:<password>@lysaccshop.a8l48kp.mongodb.net/");
+$dbName = "lysaccshop"; // Gantikan dengan nama database Anda
+?>
+
 <h2>Data Pembelian</h2>
 
 <table class="table table-bordered">
@@ -7,20 +13,39 @@
 			<th>Nama Pelanggan</th>
 			<th>Tanggal</th>
 			<th>Total</th>
-			<th>aksi</th>
+			<th>Aksi</th>
 		</tr>
 	</thead>
 	<tbody>
-	<?php $nomor=1; ?>
-	<?php $ambil=$koneksi->query("SELECT * FROM pembelian JOIN pelanggan ON pembelian.id_pelanggan=pelanggan.id_pelanggan");  ?>
-	<?php while($pecah=$ambil->fetch_assoc()){ ?>
+	<?php $nomor = 1; ?>
+	<?php
+	// Menggunakan query untuk mendapatkan data pembelian dan nama pelanggan terkait
+	$pipeline = [
+		'$lookup' => [
+			'from' => 'pelanggan', // Koleksi pelanggan
+			'localField' => 'id_pelanggan',
+			'foreignField' => 'id_pelanggan',
+			'as' => 'pelanggan'
+		]
+	];
+
+	$query = new MongoDB\Driver\Command([
+		'aggregate' => 'pembelian',
+		'pipeline' => $pipeline,
+		'cursor' => new stdClass
+	]);
+
+	$rows = $manager->executeCommand($dbName, $query);
+
+	foreach ($rows as $pecah) {
+	?>
 	<tr>
 		<td><?php echo $nomor; ?></td>
-		<td><?php echo $pecah['nama_pelanggan']; ?></td>
-		<td><?php echo $pecah['tanggal_pembelian']; ?></td>
-		<td><?php echo $pecah['total_pembelian']; ?></td>
+		<td><?php echo $pecah->pelanggan[0]->nama_pelanggan; ?></td>
+		<td><?php echo $pecah->tanggal_pembelian; ?></td>
+		<td><?php echo $pecah->total_pembelian; ?></td>
 		<td>
-			<a href="index.php?halaman=detail&id=<?php echo $pecah['id_pembelian']; ?>"class="btn btn-info">detail</a>
+			<a href="index.php?halaman=detail&id=<?php echo $pecah->_id; ?>" class="btn btn-info">detail</a>
 		</td>	
 	</tr>
 	<?php $nomor++; ?>

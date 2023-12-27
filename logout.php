279979402726edfbb61fc9933bb2d76896279061
alyssadva <128569970+alyssadva@users.noreply.github.com>
2023-12-27 16:33:00 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,10 @@
-<?php session_destroy();
-echo "<script>alert('anda telah logout');</script>";
+<?php 
+session_start(); // Mulai sesi jika belum dimulai
+
+// Menghancurkan sesi
+session_destroy();
+
+// Memberikan pesan dan mengarahkan pengguna ke halaman login
+echo "<script>alert('Anda telah logout');</script>";
 echo "<script>location='login.php';</script>";
- ?>
+?>

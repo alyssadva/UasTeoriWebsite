8b31697204ab5f3493a8ec5e708dcadc8d7b0a07
alyssadva <128569970+alyssadva@users.noreply.github.com>
2023-12-27 16:34:11 +0700

    Update pelanggan.php

--- pelanggan.php
+++ pelanggan.php
@@ -1,3 +1,9 @@
+<?php
+// Menghubungkan ke database MongoDB
+$manager = new MongoDB\Driver\Manager("mongodb+srv://alyssadiva:<password>@lysaccshop.a8l48kp.mongodb.net/");
+$dbName = "lysaccshop";
+?>
+
 <h2>Data Pelanggan</h2>
 
 <table class="table table-bordered">
@@ -12,17 +18,22 @@
 		</tr>
 	</thead>
 	<tbody>
-	<?php $nomor=1; ?>
-	<?php $ambil=$koneksi->query("SELECT * FROM pelanggan");  ?>
-	<?php while($pecah=$ambil->fetch_assoc()){ ?>
+	<?php $nomor = 1; ?>
+	<?php
+	// Menggunakan query untuk mendapatkan semua data pelanggan
+	$query = new MongoDB\Driver\Query([]); // Mengambil semua data, Anda mungkin perlu menambahkan filter atau sortir jika diperlukan
+	$rows = $manager->executeQuery("$dbName.pelanggan", $query); // Gantikan 'pelanggan' dengan nama koleksi Anda
+
+	foreach ($rows as $pecah) {
+	?>
 	<tr>
 		<td><?php echo $nomor; ?></td>
-		<td><?php echo $pecah['nama_pelanggan']; ?></td>
-		<td><?php echo $pecah['email_pelanggan']; ?></td>
-		<td><?php echo $pecah['telepon_pelanggan']; ?></td>
-		<td><?php echo $pecah['alamat_rumah']; ?></td>
+		<td><?php echo $pecah->nama_pelanggan; ?></td>
+		<td><?php echo $pecah->email_pelanggan; ?></td>
+		<td><?php echo $pecah->telepon_pelanggan; ?></td>
+		<td><?php echo $pecah->alamat_rumah; ?></td>
 		<td>
-			<a href="index.php?halaman=hapuspelanggan&id= <?php echo $pecah['id_pelanggan']; ?>"class="btn btn-danger">hapus</a>
+			<a href="index.php?halaman=hapuspelanggan&id=<?php echo $pecah->_id; ?>" class="btn btn-danger">hapus</a> <!-- Disarankan menggunakan _id sebagai identifikasi unik di MongoDB -->
 		</td>	
 	</tr>
 	<?php $nomor++; ?>
